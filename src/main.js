9eb1f7155fa508703caa3966946465d68e01bb4d
Shocco31 <oceane.rocco@gmail.com>
2020-12-13 19:07:44 +0100

    speed umb + paths

--- src/main.js
+++ src/main.js
@@ -2,33 +2,45 @@ function appuiTouche(event) {
     console.log(width);
     var gf = document.getElementById("gamefield");
     console.log(gf.offsetWidth)
+
     if(event.keyCode == 90 || event.keyCode == 38) {
     console.log("tu as appuyé sur z !")
     gsap.to("#umbreon", {
-
+      duration: 0.2,
         y:"-=" + (gf.offsetWidth / width)
     })
     
   }
-    if(event.keyCode == 83) {
+    if(event.keyCode == 83 || event.keyCode == 40) {
     console.log("tu as appuyé sur s !")
     gsap.to("#umbreon", {
-
+      duration: 0.2,
         y:"+=" + (gf.offsetWidth / width)
     })
   }
-    if(event.keyCode == 81) {
+    if(event.keyCode == 81 || event.keyCode == 37) {
     console.log("tu as appuyé sur q !")
     gsap.to("#umbreon", {
-
+      duration: 0.2,
         x:"-=" + (gf.offsetWidth / width)
     })
   }
-    if(event.keyCode == 68) {
+    if(event.keyCode == 68 || event.keyCode == 39) {
     console.log("tu as appuyé sur d !")
     gsap.to("#umbreon", {
-
+      duration: 0.2,
         x:"+=" + (gf.offsetWidth / width)
     })
   }
 }
+
+//  animation 1
+gsap.to("#james_path", {
+  duration: 2,
+  motionPath: {
+    path: "#james",
+    align: "#james"
+  },
+  ease: Linear.easeNone,
+  repeat: -1
+});
